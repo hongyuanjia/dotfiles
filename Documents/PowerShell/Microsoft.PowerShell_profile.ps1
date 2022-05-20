379cbd5356e5593e6f078980d4da4b4088417169
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2022-05-20 10:30:19 +0800

    ps: More error checking logics

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,35 +1,60 @@
 # Set a permanent Environment variable, and reload it into $env
-function Set-Environment([String] $variable, [String] $value) {
+function Set-EnvironmentVariable([String] $variable, [String] $value) {
     Set-ItemProperty "HKCU:\Environment" $variable $value
     # Manually setting Registry entry. SetEnvironmentVariable is too slow because of blocking HWND_BROADCAST
     #[System.Environment]::SetEnvironmentVariable("$variable", "$value","User")
     Invoke-Expression "`$env:${variable} = `"$value`""
 }
 
-# Make vim the default editor
-Set-Environment "EDITOR" "nvim"
-Set-Environment "GIT_EDITOR" $Env:EDITOR
+# Current version using Get-Host. Works even on PowerShell v1
+$PSVer = (Get-Host).Version
 
 # Set useful alias
-Set-Alias vim nvim
-Set-Alias cm chezmoi
+if (Get-Command nvim -ErrorAction SilentlyContinue | Test-Path) {
+    Set-Alias vim nvim
+    # Make neovim the default editor
+    Set-EnvironmentVariable "EDITOR" "nvim"
+    Set-EnvironmentVariable "GIT_EDITOR" $Env:EDITOR
+}
+
+if (Get-Command chezmoi -ErrorAction SilentlyContinue | Test-Path) {
+    Set-Alias cm chezmoi
+    # Helper to quick jump to chezmoi source dir
+    ${function:cm-cd} = { Set-Location $HOME\.local\share\chezmoi }
+}
+
 # Remove powershell default alias to enable to run R by tying 'r'
-Remove-Item Alias:\\r
-# Helper to quick jump to chezmoi source dir
-${function:cm-cd} = { Set-Location $HOME\.local\share\chezmoi }
+Remove-Alias \\r -Force -ErrorAction SilentlyContinue
 
 # Invoke starship
-Invoke-Expression (&starship init powershell)
+if (Get-Command starship -ErrorAction SilentlyContinue | Test-Path) {
+    Invoke-Expression (&starship init powershell)
+}
 
-Import-Module -Name PSReadLine
-Import-Module -Name Terminal-Icons
-Import-Module -Name scoop-completion
+if ((Get-Module -ListAvailable PSReadLine -ErrorAction SilentlyContinue) -ne $NULL) {
+    Import-Module -Name PSReadLine
+    Set-PSReadLineOption -Colors @{ InlinePrediction = '#000055'}
+    Set-PSReadLineOption -EditMode Windows
 
-Set-PSReadLineOption -Colors @{ InlinePrediction = '#000055'}
-Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-Set-PSReadLineOption -EditMode Windows
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-Set-PSReadLineKeyHandler -chord Ctrl+o -function ViForwardChar
-Set-PSReadlineKeyHandler -key Ctrl+d -function ViExit
-Set-PSReadLineKeyHandler -Chord UpArrow -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Chord DownArrow -Function HistorySearchForward
+    if ($PSVer.Major -gt 7 -and $PSVer.Minor -ge 2) {
+        Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+    } else {
+        Set-PSReadLineOption -PredictionSource History
+    }
+    Set-PSReadLineOption -PredictionViewStyle ListView
+
+    Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+    Set-PSReadlineKeyHandler -Chord Ctrl+l -Function AcceptNextSuggestionWord
+    Set-PSReadLineKeyHandler -Chord UpArrow -Function HistorySearchBackward
+    Set-PSReadLineKeyHandler -Chord DownArrow -Function HistorySearchForward
+    Set-PSReadlineKeyHandler -Chord Ctrl+p -Function PreviousSuggestion
+    Set-PSReadlineKeyHandler -Chord Ctrl+n -Function NextSuggestion
+}
+
+if ((Get-Module -ListAvailable Terminal-Icons -ErrorAction SilentlyContinue) -ne $NULL) {
+    Import-Module -Name Terminal-Icons
+}
+
+if ((Get-Module -ListAvailable scoop-completion -ErrorAction SilentlyContinue) -ne $NULL) {
+    Import-Module -Name scoop-completion
+}
