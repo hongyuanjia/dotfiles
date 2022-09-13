d746d32b3689f21d18e4f8346de518abe73fc36f
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2022-09-13 09:08:32 +0800

    nvim: update nvim-cmp config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -650,7 +650,7 @@ packer.startup(function(use)
             cmp.setup({
                 snippet = {
                     expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
+                        luasnip.lsp_expand(args.body)
                     end
                 },
                 mapping = {
@@ -746,12 +746,12 @@ packer.startup(function(use)
                 }
             })
 
-            cmp.setup.cmdline(':', {
+            cmp.setup.cmdline(":", {
                 mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources(
-                    { { name = 'path' } },
-                    { { name = 'cmdline' } }
-                )
+                sources = cmp.config.sources({
+                    { name = "path" },
+                    { name = "cmdline" }
+                })
             })
         end
     }
