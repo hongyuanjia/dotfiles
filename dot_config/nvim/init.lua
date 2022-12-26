9340c0a955f0f37f97f5ca7dd24370fba5583e41
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2022-12-26 17:00:26 +0800

    nvim: lazy load colorscheme

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -342,10 +342,11 @@ lazy.setup({
     -- colorscheme
     {
         "folke/tokyonight.nvim",
-        lazy = false,
+        init = function()
+            vim.cmd.colorscheme("tokyonight")
+        end,
         config = function()
             require("tokyonight").setup()
-            vim.cmd.colorscheme("tokyonight")
         end
     },
 
