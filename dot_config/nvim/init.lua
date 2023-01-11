83c3101695a1615eb61e21c87705a0f782db16c9
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2023-01-11 22:46:44 +0800

    nvim: add xmllint

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -988,10 +988,12 @@ lazy.setup({
             })
             local lspconfig = require("lspconfig")
 
-            require("null-ls").setup({
+            local null_ls = require("null-ls")
+            null_ls.setup({
                 sources = {
-                    require("null-ls").builtins.completion.spell,
-                    require("null-ls").builtins.code_actions.gitsigns
+                    null_ls.builtins.completion.spell,
+                    null_ls.builtins.code_actions.gitsigns,
+                    null_ls.builtins.formatting.xmllint
                 }
             })
 
