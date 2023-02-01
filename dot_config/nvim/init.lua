232a51da440f202a3afde84469adef6d1b76147e
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2023-02-01 17:56:01 +0800

    nvim: remove vim-matchup

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1467,13 +1467,6 @@ lazy.setup({
         end,
     },
     { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
-    {
-        "andymass/vim-matchup",
-        event = "BufReadPost",
-        config = function()
-            vim.g.matchup_matchparen_offscreen = { method = "status_manual" }
-        end
-    },
 
     -- Git
     {
