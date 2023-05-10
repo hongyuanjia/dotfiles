3efa58368c740e5344f15bd3bd5188701f84e92b
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2023-05-10 15:00:57 +0800

    nvim: update nvim-tree config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1386,14 +1386,15 @@ lazy.setup({
                     quit_on_open = true
                 }
             },
-            view = {
-                mappings = {
-                    list = {
-                        { key = "h", action = "close_node" },
-                        { key = "l", action = "edit"}
-                    }
-                }
-            }
+            on_attach = function(bufnr)
+                local api = require('nvim-tree.api')
+                local function opts(desc)
+                    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+                end
+                api.config.mappings.default_on_attach(bufnr)
+                vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
+                vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+            end
         }
     },
 
