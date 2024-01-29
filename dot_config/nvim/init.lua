81b482b77fbc4649b7a178284906eab960561c9e
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2024-01-29 23:04:34 +0800

    nvim: only show single mosst sign for diagnostics

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1087,6 +1087,36 @@ lazy.setup({
                 vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
             end
 
+            -- custom namespace
+            local ns_diag = vim.api.nvim_create_namespace('severe-diagnostics')
+
+            -- reference to the original handler
+            local orig_signs_handler = vim.diagnostic.handlers.signs
+
+            -- only show the single most relevant sign for diagnostics
+            vim.diagnostic.handlers.signs = {
+                show = function(_, bufnr, _, opts)
+                    -- get all diagnostics from the whole buffer rather
+                    -- than just the diagnostics passed to the handler
+                    local diagnostics = vim.diagnostic.get(bufnr)
+
+                    -- find the "worst" diagnostic per line
+                    local max_severity_per_line = {}
+                    for _, d in pairs(diagnostics) do
+                        local m = max_severity_per_line[d.lnum]
+                        if not m or d.severity < m.severity then
+                            max_severity_per_line[d.lnum] = d
+                        end
+                    end
+
+                    local filtered_diagnostics = vim.tbl_values(max_severity_per_line)
+                    orig_signs_handler.show(ns_diag, bufnr, filtered_diagnostics, opts)
+                end,
+                hide = function(_, bufnr)
+                    orig_signs_handler.hide(ns_diag, bufnr)
+                end
+            }
+
             -- use virtual text
             vim.diagnostic.config({
                 underline = true,
