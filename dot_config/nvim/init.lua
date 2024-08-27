aa24ab6f40e6f188e4b3bc8bc8f3ed873be3bfab
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2024-08-27 09:08:59 +0800

    nvim: setup nvim-dap

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -5,7 +5,7 @@
 --
 --
 -- Author: @hongyuanjia
--- Last Modified: 2024-08-13 00:49
+-- Last Modified: 2024-08-24 16:05
 
 -- Basic Settings
 local options = {
@@ -1225,6 +1225,94 @@ lazy.setup({
             { "gr", "<cmd>Trouble lsp_references<CR>", desc = "Trouble: List references" },
         },
         opts = { use_diagnostic_signs = true }
+    },
+
+    -- dap
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "rcarriga/nvim-dap-ui",
+            -- virtual text for the debugger
+            { "theHamsta/nvim-dap-virtual-text", opts = {} },
+            "jay-babu/mason-nvim-dap.nvim"
+        },
+
+        keys = {
+            { "<Leader>d", "", desc = "+debug", mode = {"n", "v"} },
+            { "<Leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
+            { "<Leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+            { "<Leader>dc", function() require("dap").continue() end, desc = "Continue" },
+            { "<Leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+            { "<Leader>dg", function() require("dap").goto_() end, desc = "Go to Line (No Execute)" },
+            { "<Leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+            { "<Leader>dj", function() require("dap").down() end, desc = "Down" },
+            { "<Leader>dk", function() require("dap").up() end, desc = "Up" },
+            { "<Leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+            { "<Leader>do", function() require("dap").step_out() end, desc = "Step Out" },
+            { "<Leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
+            { "<Leader>dp", function() require("dap").pause() end, desc = "Pause" },
+            { "<Leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+            { "<Leader>ds", function() require("dap").session() end, desc = "Session" },
+            { "<Leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
+            { "<Leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+        },
+
+        config = function()
+            require ('mason-nvim-dap').setup({
+                ensure_installed = {'stylua', 'jq'},
+                handlers = {}, -- sets up dap in the predefined manner
+            })
+
+            vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
+
+            -- setup dap config by VsCode launch.json file
+            local vscode = require("dap.ext.vscode")
+            local json = require("plenary.json")
+            vscode.json_decode = function(str)
+                return vim.json.decode(json.json_strip_comments(str))
+            end
+
+            -- Extends dap.configurations with entries read from .vscode/launch.json
+            if vim.fn.filereadable(".vscode/launch.json") then
+                vscode.load_launchjs()
+            end
+        end,
+    },
+    -- fancy UI for the debugger
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = { "nvim-neotest/nvim-nio" },
+        keys = {
+            { "<leader>du", function() require("dapui").toggle({}) end, desc = "Dap UI" },
+            { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+        },
+        opts = {},
+        config = function(_, opts)
+            local dap = require("dap")
+            local dapui = require("dapui")
+            dapui.setup(opts)
+            dap.listeners.after.event_initialized["dapui_config"] = function()
+                dapui.open({})
+            end
+            dap.listeners.before.event_terminated["dapui_config"] = function()
+                dapui.close({})
+            end
+            dap.listeners.before.event_exited["dapui_config"] = function()
+                dapui.close({})
+            end
+        end,
+    },
+    -- mason.nvim integration
+    {
+        "jay-babu/mason-nvim-dap.nvim",
+        dependencies = "mason.nvim",
+        cmd = { "DapInstall", "DapUninstall" },
+        opts = {
+            automatic_installation = true,
+            handlers = {},
+            ensure_installed = {},
+        },
+        config = function() end,
     },
 
     -- Telescope
